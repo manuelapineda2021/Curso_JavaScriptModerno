cad4099e8420146262f447a1301e4a97065d206c
mpineda@pratechgroup.com <mpineda@pratechgroup.com>
2022-06-23 22:32:56 -0500

    se leen los select del formulario y se filtra por marca

--- 17-Proyecto-Buscador/js/app.js
+++ 17-Proyecto-Buscador/js/app.js
@@ -1,9 +1,28 @@
 //Variables
-const resultado = document.querySelector('#resultado');
+const marca = document.querySelector('#marca');
 const year = document.querySelector('#year');
+const minimo = document.querySelector('#minimo');
+const maximo= document.querySelector('#maximo');
+const puertas= document.querySelector('#puertas');
+const transmision= document.querySelector('#transmision');
+const color = document.querySelector('#color')
+
+//Contenedor para los resultados
+const resultado = document.querySelector('#resultado');
 
 const max = new Date().getFullYear(); //año actual
 const min = max - 10;
+
+//Generar un objeto con la busqueda
+const datosBusqueda = {
+    marca: '',
+    year: '',
+    minimo: '',
+    maximo: '',
+    puertas: '',
+    transmision: '',
+    color: ''
+}
 
 //Eventos
 //Cargue HTML
@@ -11,6 +30,36 @@ document.addEventListener('DOMContentLoaded', () =>{
 
     mostrarAutos(); //muestra los autos al cargar
     llegarSelect();//Llena las opciones de años
+});
+
+//Event listener para los select de la busqueda
+marca.addEventListener('change', (event) =>{
+    datosBusqueda.marca = event.target.value;
+    filtrarAuto();
+});
+
+year.addEventListener('change', (event) =>{
+    datosBusqueda.year = event.target.value;
+});
+
+minimo.addEventListener('change', (event) =>{
+    datosBusqueda.minimo = event.target.value;
+});
+
+maximo.addEventListener('change', (event) =>{
+    datosBusqueda.maximo = event.target.value;
+});
+
+puertas.addEventListener('change', (event) =>{
+    datosBusqueda.puertas = event.target.value;
+});
+
+transmision.addEventListener('change', (event) =>{
+    datosBusqueda.transmision = event.target.value;
+});
+
+color.addEventListener('change', (event) =>{
+    datosBusqueda.color = event.target.value;
 });
 
 
@@ -40,4 +89,19 @@ function llegarSelect(){
         opcion.textContent = i;
         year.appendChild(opcion); //Agrega las opciones del año al select
     }
+}
+
+//Filtra con base a la busqueda
+function filtrarAuto(){
+    const resultado = autos.filter(filtrarMarca);
+    console.log(resultado);
+}
+
+function filtrarMarca(auto){
+    const {marca} = datosBusqueda;
+    if(marca){
+        //filtro los que tienen esa marca
+        return auto.marca === marca;
+    }
+    return auto; //retorna automovil completo
 }
